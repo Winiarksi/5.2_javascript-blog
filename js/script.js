7db0501f598b0fa3b18c834c1c9c7a3dbfda851b
Artur Winiarski <winiarski.a.r@gmail.com>
2020-08-24 21:32:15 +0200

    remove class 'active' from all articles && remove class 'active' from all article links

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
 'use strict';
 
-function titleClickHandler(event){
+function titleClickHandler(event) {
   console.log('Link was clicked!');
   console.log(event);
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+  for (let activeLink of activeLinks) {
+
+    if (activeLink.classList.contains('active')) {
+      console.log("removing the active class in class title class");
+      activeLink.classList.remove('active');
+    }
+
+  }
+
 
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
-
+  
+  const activeArticles = document.querySelectorAll('.posts article.active')
+  for (let activeArticle of activeArticles) {
+    if(activeArticle.classList.contains('active')){
+      console.log("removing the active class  in class post class");
+      activeArticle.classList.remove('active');
+    }
+  }
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
@@ -18,6 +35,6 @@ function titleClickHandler(event){
 
 const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
+for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
