308bb0b323cc7e8e45f9ec0c01aa443f19ace47e
Artur Winiarski <winiarski.a.r@gmail.com>
2020-08-30 20:03:50 +0200

    generate title links: remove contents of titleList

--- js/script.js
+++ js/script.js
@@ -56,22 +56,24 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
 
-  function generateTitleLinks(){
+function generateTitleLinks() {
 
-    /* remove contents of titleList */
-  
-    /* for each article */
-  
-      /* get the article id */
-  
-      /* find the title element */
-  
-      /* get the title from the title element */
-  
-      /* create HTML of the link */
-  
-      /* insert link into titleList */
-  
-  }
-  
-  generateTitleLinks();
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleSelector);
+  titleList.innerHTML = "";
+
+  /* for each article */
+
+  /* get the article id */
+
+  /* find the title element */
+
+  /* get the title from the title element */
+
+  /* create HTML of the link */
+
+  /* insert link into titleList */
+
+}
+
+generateTitleLinks();
